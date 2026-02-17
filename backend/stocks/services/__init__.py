b87f8f19d2efe4b435e62cc6bff37b85b7675ac8
mymadhavyadav07 <mymadhavyadav07@gmail.com>
2026-02-17 19:51:32 +0530

    Initial commit

--- backend/stocks/services/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Backend services for stocks and currency operations.
-"""
-
-from .stock_service import get_stock_service
-from .currency_service import CurrencyExchangeService
-
-__all__ = ['get_stock_service', 'CurrencyExchangeService']
